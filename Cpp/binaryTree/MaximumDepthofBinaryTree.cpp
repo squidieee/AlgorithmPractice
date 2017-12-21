771518670b826a67ea95bfddd470623e00abfe6c
qzhou <qzhou@ece.ubc.ca>
2017-12-21 00:28:19 -0800

    .

--- Cpp/binaryTree/MaximumDepthofBinaryTree.cpp
+++ Cpp/binaryTree/MaximumDepthofBinaryTree.cpp
@@ -12,15 +12,80 @@ The maximum depth is the number of nodes along the longest path from the root no
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
  class Solution {
 public:
+    /* 
+    // post-order depth-first search: recursive
     int maxDepth(TreeNode* root) {
         if (root ==NULL)
             return 0;
-        if(root->left ==NULL && root->right==NULL)
-            return 1;
-        int nl = maxDepth(root->left);
-        int nr = maxDepth(root->right);
-        return max(nl,nr)+1;
+        return max(maxDepth(root->left),maxDepth(root->right))+1;
+    }
+    
+    
+    // breath-first search: iterative
+    int maxDepth(TreeNode* root)
+    {
+        if (root==NULL)
+            return 0;
+        
+        queue<TreeNode*> q; // stores all nodes on a level
+        q.push(root); // level 0
+        int dep(0);
+        while(!q.empty()){ // for each level
+            int len = q.size(); // number of nodes on this level
+            for(int i = 0; i < len; i++){ // for each node on this level
+                TreeNode* node = q.front(); 
+                q.pop();
+                if(node->left != NULL)
+                    q.push(node->left);
+                if(node->right!=NULL)
+                    q.push(node->right);
+            }
+            dep++;
+        }
+        return dep;
+    }
+    */
+    
+    // depth-first search: iterative
+    int maxDepth(TreeNode* root)
+    {
+        if(root==NULL)
+            return 0;
+        stack<TreeNode*> s;
+        stack<int> dep;
+        s.push(root);
+        dep.push(1);
+        int maxdep(0);
+        while(!s.empty())
+        {
+            TreeNode* node = s.top();
+            s.pop(); 
+            int level = dep.top(); 
+            dep.pop();
+            maxdep = max(maxdep, level);
+
+            if(node->right != NULL)
+            {
+                s.push(node->right); 
+                dep.push(level+1);
+            }
+            if(node->left != NULL)
+            {
+                s.push(node->left);
+                dep.push(level+1);
+            }    
+        }
+        return maxdep;
     }
 };
--- Cpp/binaryTree/MinimumDepthofBinaryTree.cpp
+++ Cpp/binaryTree/MinimumDepthofBinaryTree.cpp
@@ -11,8 +11,19 @@ The minimum depth is the number of nodes along the shortest path from the root n
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
  class Solution {
 public:
+    /* 
+    // depth-first search: recursive
     int minDepth(TreeNode* root) {
         if (root==NULL)
             return 0;
@@ -22,5 +33,31 @@ public:
             return minDepth(root->left) +1;
         else
             return min(minDepth(root->left), minDepth(root->right)) + 1;
+    }
+    */
+    // breadth-first search: iteratively
+    int minDepth(TreeNode* root)
+    {
+        if (root==NULL) return 0;
+        queue<TreeNode*> q;
+        q.push(root);
+        int dep(0);
+        while(!q.empty())
+        {
+            dep++;
+            int len = q.size();
+            for (int i = 0; i < len; i++)
+            {
+                TreeNode* node = q.front();
+                q.pop();
+                if(node->left==NULL&&node->right==NULL)
+                    return dep;
+                if(node->left!=NULL)
+                    q.push(node->left);
+                if(node->right!=NULL)
+                    q.push(node->right);
+            }
+        }
+        return dep;
     }
 };
