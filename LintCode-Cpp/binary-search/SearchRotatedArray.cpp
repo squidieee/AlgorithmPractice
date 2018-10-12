e7c405f1718d077ae28a8c4cd958c344590182c4
qzhou <qzhou@ece.ubc.ca>
2018-10-12 16:13:08 -0700

    .

---
+++ LintCode-Cpp/binary-search/SearchRotatedArray.cpp
@@ -0,0 +1,67 @@
+/* 
+62. Search in Rotated Sorted Array
+Suppose a sorted array is rotated at some pivot unknown to you beforehand.
+
+(i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+
+You are given a target value to search. If found in the array return its index, otherwise return -1.
+
+You may assume no duplicate exists in the array.
+
+Example
+For [4, 5, 1, 2, 3] and target=1, return 2.
+
+For [4, 5, 1, 2, 3] and target=0, return -1.
+
+Challenge
+O(logN) time
+*/
+class Solution {
+public:
+    /**
+     * @param A: an integer rotated sorted array
+     * @param target: an integer to be searched
+     * @return: an integer
+     */
+    int search(vector<int> &A, int target) {
+        if (A.empty()) return -1;
+        
+        // step1: find minimum index
+        int start = findMin(A);
+        int end;
+        // step2: find target in SubArray
+        if (start == 0) // non-rotated array
+        {
+            end = A.size()-1;
+        }
+        else // rotated array
+        {
+            if (target >= A[start] && target <= A[A.size()-1])
+                end = A.size()-1;
+            else if (target >= A[0] && target <= A[start - 1])
+            {
+                end = start - 1;
+                start = 0;
+            }
+            else
+                return -1;
+        }
+        
+        while(start + 1 < end)
+        {
+            int mid = start + (end - start)/2;
+            if (target > A[mid])
+            {
+                start = mid;
+            }
+            else
+            {
+                end = mid;
+            }
+        }
+        
+        if (A[start]==target) return start;
+        if (A[end]==target) return end;
+        return -1;
+        
+    }
